08f776aeb06396a4be6b6d7108f4ec9ab9f387fd
Paulo Barreto <paulovgtb@gmail.com>
2023-08-04 06:43:46 +0100

    creates loading component

---
+++ src/app/components/loading/loading.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.scss'],
+})
+export class LoadingComponent {}
